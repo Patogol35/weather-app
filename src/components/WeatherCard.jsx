712d37aeb0d15c200a9a8ba4e72f6f3db0a5ba07
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-08-24 17:29:55 -0500

    Update WeatherCard.jsx

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,17 +1,16 @@
 function WeatherCard({ weather }) {
   return (
-    <div className="bg-white text-gray-800 rounded-xl shadow-xl p-6 w-80 text-center">
-      <h2 className="text-2xl font-bold mb-2">
+    <div className="card">
+      <h2>
         {weather.name}, {weather.sys.country}
       </h2>
       <img
         src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
         alt="icon"
-        className="mx-auto"
       />
-      <p className="text-4xl font-bold">{weather.main.temp}°C</p>
-      <p className="capitalize text-lg">{weather.weather[0].description}</p>
-      <div className="mt-4">
+      <p className="temp">{Math.round(weather.main.temp)}°C</p>
+      <p className="desc">{weather.weather[0].description}</p>
+      <div className="extra">
         <p>💧 Humedad: {weather.main.humidity}%</p>
         <p>🌬️ Viento: {weather.wind.speed} m/s</p>
       </div>
@@ -20,4 +19,3 @@ function WeatherCard({ weather }) {
 }
 
 export default WeatherCard;
-
