14c55b1bfb5715305f18d266847f6d481f3e80d6
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-08-24 17:24:33 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./App.css";
 import axios from "axios";
 import WeatherCard from "./components/WeatherCard";
 import ForecastCard from "./components/ForecastCard";
