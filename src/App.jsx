21da88c702ed8e59222b27587e6720acf189acab
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-10-01 23:56:49 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react";
+import useWeather from "./hooks/useWeather";
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import Home from "./pages/Home";
 
 function App() {
+  const [city, setCity] = useState("");
   const [darkMode, setDarkMode] = useState(() => {
     const savedMode = localStorage.getItem("darkMode");
     return savedMode ? savedMode === "true" : false;
   });
 
-  // Persistencia de modo oscuro
+  const { weather, forecast, loading, error, fetchWeather } = useWeather();
+
+  // Persistencia modo oscuro
   useEffect(() => {
     const savedMode = localStorage.getItem("darkMode");
     if (savedMode) setDarkMode(savedMode === "true");
@@ -18,14 +22,14 @@ function App() {
     localStorage.setItem("darkMode", darkMode);
   }, [darkMode]);
 
+  const handleSearch = () => fetchWeather(city);
+  const handleKeyDown = (e) => e.key === "Enter" && handleSearch();
+
   const theme = createTheme({
     palette: {
       mode: darkMode ? "dark" : "light",
       ...(darkMode && {
-        background: {
-          default: "#121212",
-          paper: "#1e1e1e",
-        },
+        background: { default: "#121212", paper: "#1e1e1e" },
       }),
     },
   });
@@ -33,8 +37,19 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      {/* Aquí cargas la página principal */}
-      <Home darkMode={darkMode} setDarkMode={setDarkMode} theme={theme} />
+      <Home
+        city={city}
+        setCity={setCity}
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+        handleSearch={handleSearch}
+        handleKeyDown={handleKeyDown}
+        weather={weather}
+        forecast={forecast}
+        loading={loading}
+        error={error}
+        theme={theme}
+      />
     </ThemeProvider>
   );
 }
