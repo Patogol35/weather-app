5f7db012e0ad2b3c378c652e6cedc6355867e906
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-10-01 23:57:17 -0500

    Update appStyles.js

--- src/styles/appStyles.js
+++ src/styles/appStyles.js
@@ -1,42 +1,40 @@
-// src/styles/appStyles.js
-export const appStyles = {
-  root: (darkMode, theme) => ({
+export const styles = {
+  app: {
     minHeight: "100vh",
-    backgroundColor: darkMode ? theme.palette.background.default : undefined,
-    color: darkMode ? theme.palette.text.primary : undefined,
     transition: "all 0.4s ease",
-  }),
-
-  darkModeBtn: {
-    "&:hover": { backgroundColor: "transparent" },
-    "&:focus": { outline: "none" },
-    transition: "color 0.3s ease",
   },
-
-  header: {
+  headerRight: {
+    display: "flex",
+    justifyContent: "flex-end",
+    padding: "1rem 1rem 0 1rem",
+  },
+  headerLeft: {
     display: "flex",
     justifyContent: "flex-start",
     alignItems: "center",
     gap: "1rem",
     padding: "0 1rem",
   },
-
   subtitle: {
     padding: "0 1rem",
   },
-
-  searchBox: {
+  search: {
     display: "flex",
     gap: "8px",
     padding: "0 1rem",
   },
-
+  searchButton: {
+    textTransform: "none",
+    fontWeight: "bold",
+    borderRadius: "8px",
+  },
   error: {
     padding: "0 1rem",
     color: "red",
   },
-
-  loading: {
-    padding: "0 1rem",
+  iconButton: {
+    "&:hover": { backgroundColor: "transparent" },
+    "&:focus": { outline: "none" },
+    transition: "color 0.3s ease",
   },
 };
