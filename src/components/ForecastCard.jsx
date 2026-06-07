f38bbe34cc45846ead5c275795a32d96ace17555
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-06-07 16:00:33 -0500

    Update ForecastCard.jsx

--- src/components/ForecastCard.jsx
+++ src/components/ForecastCard.jsx
@@ -1,18 +1,25 @@
-export default function ForecastCard({ day, minTemp, maxTemp, icon, description }) {
+export default function ForecastCard({ data }) {
+  const date = new Date(data.dt_txt);
+  const day = date.toLocaleDateString("es-ES", {
+    weekday: "long",
+  });
+
   return (
     <article className="card">
-      <h3 className="day">{day}</h3>
+      <h3 className="day">
+        {day.charAt(0).toUpperCase() + day.slice(1)}
+      </h3>
 
       <img
-        src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
-        alt={description}
+        src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+        alt={data.weather[0].description}
       />
 
       <p className="temp">
-        {Math.round(maxTemp)}° / {Math.round(minTemp)}°
+        {Math.round(data.main.temp_max)}° / {Math.round(data.main.temp_min)}°
       </p>
 
-      <p className="desc">{description}</p>
+      <p className="desc">{data.weather[0].description}</p>
     </article>
   );
 }
